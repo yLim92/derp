e346b638202b8f9f07b703a93453abe5682ab27e
ylim <yongjie.lim@jibjab.com>
2015-08-07 09:48:39 +0000

    Finish log in/log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember(user) if params[:session][:remember_me] == "1"
+      redirect_to user
+    else
+      flash.now[:danger] = "You suck"
+      render 'new'
+    end
+  end
+  
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   
+  attr_accessor :remember_token
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 250 }, 
@@ -8,4 +9,36 @@ class User < ActiveRecord::Base
   before_save { self.email.downcase!}
   
   has_secure_password
+  
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.blank?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  
 end
