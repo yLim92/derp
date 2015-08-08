683d417d885d7b2cbc7bb3f30984d9d95cc25cad
ylim <yongjie.lim@jibjab.com>
2015-08-08 17:34:26 +0000

    Added account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      remember(user) if params[:session][:remember_me] == "1"
-      redirect_back_or(user)
+      if user.activated?
+        log_in user
+        remember(user) if params[:session][:remember_me] == "1"
+        redirect_back_or(user)
+      else
+        message = "Account not activated. Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "You suck"
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ActiveRecord::Base
   
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  
+  before_save :downcase_email
+  before_create :create_activation_digest
+  
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 250 }, 
     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  before_save { self.email.downcase!}
   
   has_secure_password
   
@@ -34,11 +37,28 @@ class User < ActiveRecord::Base
   end
   
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.blank?
-
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.blank?
+    BCrypt::Password.new(digest).is_password?(token)
   end
   
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
   
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
+  private
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+    
+    def downcase_email
+      self.email.downcase!
+    end
 end
